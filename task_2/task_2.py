02087989caa083c24e2886ab343c6b6fe0409cab
Denis <denis-topalov@yandex.ru>
2023-08-12 13:46:59 +1100

    task_2

---
+++ task_2/task_2.py
@@ -0,0 +1,11 @@
+def print_operation_table(operation, num_rows=9, num_columns=9):
+    for i in range(1, num_rows + 1): 
+        table = []
+        for j in range(1, num_columns + 1):
+            table.append(str(operation(i, j)))
+        print("\t".join(table))
+print_operation_table(lambda x, y: x * y)
+
+
+
+                     
